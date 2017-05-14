db27904cea06c0e1d226efa6e8b6f3d65113296b
namalkanishka <you@example.com>
2017-05-14 10:10:24 +0530

    rating in booking page

--- ci_intro/application/controllers/Book.php
+++ ci_intro/application/controllers/Book.php
@@ -8,7 +8,7 @@ class Book extends CI_Controller
 			{
 		       echo '<h2> Welcome -'.$this->session->userdata('username').'</h2>';
 		       echo $des;
-		       echo "load a viwe here s "
+		       echo "load a viwe here s ";
 		       // href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');"
 		       //echo '<a href='base_url('/index.php/Mylogin/logout')'>logout</a>';
 			}
--- ci_intro/application/views/Booking.php
+++ ci_intro/application/views/Booking.php
@@ -132,6 +132,26 @@
 </head>
 
 <body>
+<?php
+	function db_connect(){
+  $server_name = "localhost";
+  $user_name = "root";
+  $password = "";
+  $db_name = "ci_b";
+  $db = new mysqli($server_name, $user_name, $password, $db_name);
+  if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+  }
+
+  return $db;
+}
+
+$db = db_connect();
+mysqli_query($db,"set character_set_results='utf8'"); 
+?>
+
+
+
 <div id="container">
  
  <div id="body">
@@ -159,9 +179,42 @@
     foreach ($dat as $object) {
     	$des= $object->Destination.'<br/>';
       	$a=$object->ImagePath;
+      	$id=$object->Rid;
     	?>
     	<div id="picA"><img src="<?php echo base_url($a);?>" width="260" height="125"></div>
      	<div id="desA"><h3><?php echo $des; ?></h3>
+     		<?php
+        	$id=$object->Rid;
+        	$sql = 'SELECT COUNT(DISTINCT(vote_id)) total_rows,IFNULL(SUM(blog_vote),0) total_rating, blog_id
+            FROM blog_vote 
+            WHERE blog_id='.$id.' LIMIT 1';
+            	$result3 = $db->query($sql);
+              	$row =$result3->fetch_assoc();
+		        
+		        $total_rows = $row['total_rows'];
+		        $total_rating = $row['total_rating'];
+		        $results['vote_rows'] = $total_rows;
+		        $rating = 0;
+		        if ($total_rows > 0) {
+		            $rating = $total_rating / $total_rows;
+		        }
+		        $dec_rating = round($rating, 1);
+		        //echo  $dec_rating;
+
+                $starNumber=$dec_rating;
+                for($x=1;$x<=$starNumber;$x++) {
+                    echo '<img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-128.png" style="width:35px" />';
+                }
+                if (strpos($starNumber,'.')) {
+                    echo '<img src="https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Star-Half-Full.png"  style="width:35px"/>';
+                    $x++;
+                }
+                while ($x<=5) {
+                    echo '<img src="http://time-static-shared.s3-website-us-east-1.amazonaws.com/interactives/how_american_are_you/images/white-star-md.png" style="width:35px" />';
+                    $x++;
+                }
+           
+			?>
      	</div>
 
      	<div id= "priceA">
@@ -169,7 +222,8 @@
         <p></p>
         
         <p>
-         <input type="button" id="BtnA" value=" Book Now " >
+        	<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/index/'.$des) ?>">Book Now </a>	
+         
        </p>
      </div>
      </div>
@@ -184,14 +238,51 @@
     foreach ($ava as $object1) {
     	$des= $object1->Destination;
       	$a=$object1->ImagePath;
+      	$id=$object1->Rid;
     	?>
     	<div id="picA"><img src="<?php echo base_url($a);?>" width="260" height="125"></div>
      	<div id="desA"><h3><?php echo $des; ?></h3>
+     	<?php
+        	$id=$object1->Rid;
+        	$sql = 'SELECT COUNT(DISTINCT(vote_id)) total_rows,IFNULL(SUM(blog_vote),0) total_rating, blog_id
+            FROM blog_vote 
+            WHERE blog_id='.$id.' LIMIT 1';
+            	$result3 = $db->query($sql);
+              	$row =$result3->fetch_assoc();
+		        
+		        $total_rows = $row['total_rows'];
+		        $total_rating = $row['total_rating'];
+		        $results['vote_rows'] = $total_rows;
+		        $rating = 0;
+		        if ($total_rows > 0) {
+		            $rating = $total_rating / $total_rows;
+		        }
+		        $dec_rating = round($rating, 1);
+		        //echo  $dec_rating;
+
+                $starNumber=$dec_rating;
+                for($x=1;$x<=$starNumber;$x++) {
+                    echo '<img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-128.png" style="width:35px" />';
+                }
+                if (strpos($starNumber,'.')) {
+                    echo '<img src="https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Star-Half-Full.png"  style="width:35px"/>';
+                    $x++;
+                }
+                while ($x<=5) {
+                    echo '<img src="http://time-static-shared.s3-website-us-east-1.amazonaws.com/interactives/how_american_are_you/images/white-star-md.png" style="width:35px" />';
+                    $x++;
+                }
+           
+			?>
+
+
      	</div>
 
      	<div id= "priceA">
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;LKR 10 800
-        <p></p>
+        <p>
+        	
+        </p>
         
         <p>
         	<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/index/'.$des) ?>">Book Now </a>	
@@ -204,7 +295,11 @@
     <?php
 
     }
+
+    
+
 	?>
+
 
 
 

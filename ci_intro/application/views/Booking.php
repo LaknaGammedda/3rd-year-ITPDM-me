66a5c97bc33e20d5af3d8b5d92ce2eda681229cb
namalkanishka <you@example.com>
2017-05-15 17:07:00 +0530

    ui cahanges to bokking php

--- ci_intro/application/views/Booking.php
+++ ci_intro/application/views/Booking.php
@@ -1,158 +1,39 @@
 <!doctype html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link href="../../../../../../../Users/adm/Downloads/Dw B&B/Dw B&B/Booking.css" rel="stylesheet" type="text/css"/>
-  <style type="text/css">
-  #container {
-	height: 768px;
-	width: 1024px;
-	margin-left:auto;
-	margin-right:auto;
-	border:1px solid;
-	border-color:#000;
-    }
-  #logo {
-	float: left;
-	height: 50px;
-	width: 75px;
-}
-#A {
-	height: 150px;
-	width: 964px;
-	margin-top: 20px;
-	margin-bottom: 5px;
-	margin-left: auto;
-	margin-right: auto;
-	border: 1px solid;
-	border-color: #000;
-	padding-top: 12px;
-	padding-bottom: 10px;
-	box-shadow: inset 4px 1px 2px 3px #999;
-}
-#B {
-	height: 150px;
-	width: 964px;
-	margin-top: 20px;
-	margin-bottom: 5px;
-	margin-left: auto;
-	margin-right: auto;
-	border: 1px solid;
-	border-color: #000;
-	padding-top: 12px;
-	padding-bottom: 10px;
-	box-shadow: inset 4px 1px 2px 3px #999;
-}
-#C {
-	height: 150px;
-	width: 964px;
-	margin-top: 20px;
-	margin-bottom: 5px;
-	margin-left: auto;
-	margin-right: auto;
-	border: 1px solid;
-	border-color: #000;
-	padding-top: 12px;
-	padding-bottom: 10px;
-	box-shadow: inset 4px 1px 2px 3px #999;
-}
-#D {
-	height: 150px;
-	width: 964px;
-	margin-top: 20px;
-	margin-bottom: 5px;
-	margin-left: auto;
-	margin-right: auto;
-	border: 1px solid;
-	border-color: #000;
-	padding-top: 12px;
-	padding-bottom: 10px;
-	box-shadow: inset 4px 1px 2px 3px #999;
-}
-  #titleBar {
-	height: 50px;
-	width: auto;
-}
-   #picA {
-	float: left;
-	height: auto;
-	width: 30%;
-	padding-left:10px;
-}
-   #desA {
-	height: auto;
-	width: auto;
-	float: left;
-	color: #036;
-}
- #priceA {
-	height: 110px;
-	width: 20%;
-	float: right;
-	padding-left: 35px;
-	padding-top: 25px;
-	padding-bottom: 20px;
-	border-left-width: inherit;
-	border-left-color: #000;
-	border-left-style: solid;
-	margin-top: 6px;
-	color: #036;
-}
-  #BtnA {
-	font-size: 17px;
-	font-weight: bold;
-	width: 125px;
-	font-family: "Times New Roman", Times, serif;
-	margin-right: 10px;
-	background-color: #FFF;
-	border-color: #666;
-	border: 1px solid;
-	border-style: solid;
-	border-radius: 4px;
-	height: 30px;
-	box-shadow: 2px 1px 1px 1px #06F;
-	padding: 2.5px;
-	color: #003;
-}
- 
- 
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="../../../../../../../Users/adm/Downloads/Dw B&B/Dw B&B/Booking.css" rel="stylesheet" type="text/css"/>
 
-  #container #body p em {
-	font-family: "Times New Roman", Times, serif;
-	font-size: 20px;
-	font-weight: 900;
-	color: #069;
-}
-  </style>
-<title>Place a Booking</title>
+
+	<title>Place a Booking</title>
 </head>
 
 <body>
-<?php
+	<?php
 	function db_connect(){
-  $server_name = "localhost";
-  $user_name = "root";
-  $password = "";
-  $db_name = "ci_b";
-  $db = new mysqli($server_name, $user_name, $password, $db_name);
-  if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-  }
+		$server_name = "localhost";
+		$user_name = "root";
+		$password = "";
+		$db_name = "ci_b";
+		$db = new mysqli($server_name, $user_name, $password, $db_name);
+		if($db->connect_errno > 0){
+			die('Unable to connect to database [' . $db->connect_error . ']');
+		}
 
-  return $db;
-}
+		return $db;
+	}
 
-$db = db_connect();
-mysqli_query($db,"set character_set_results='utf8'"); 
-?>
+	$db = db_connect();
+	mysqli_query($db,"set character_set_results='utf8'"); 
+	?>
 
 
 
-<div id="container">
+<!-- <div id="container">
  
  <div id="body">
  <p></p>
@@ -162,7 +43,7 @@ mysqli_query($db,"set character_set_results='utf8'");
 
    <div id="A">
      
-     <div id="picA"><img src="<?php echo base_url('Image/images (2).jpg');?>" width="260" height="125"></div>
+     //
      
      <div id="desA"><h3>Kandalama </h3>
       
@@ -176,152 +57,177 @@ mysqli_query($db,"set character_set_results='utf8'");
        </p>
      </div>
    </div>
+-->
+
+
+
+<table class="table">
+    <!-- <thead>
+      <tr>
+        <th></th>
+        <th></th>
+        <th>Email</th>
+      </tr>
+  </thead> -->
+  <tbody>
 
 
 
 
 
+  	<?php
+  	foreach ($dat as $object) {
+  		$des= $object->Destination.'<br/>';
+  		$a=$object->ImagePath;
+  		$name=$object->LodgeName;
+  		$id=$object->Rid;
+  		$price=$object->Price;
+  		?>
+
+
+  		<tr>
+  			<td><div id="picA"><img src="<?php echo base_url($a);?>" width="260" height="125"></div></td>
+  			<td><div id="desA"><h3><?php echo $des; ?></h3>
+
+  				<?php
+  				$id=$object->Rid;
+  				$sql = 'SELECT COUNT(DISTINCT(vote_id)) total_rows,IFNULL(SUM(blog_vote),0) total_rating, blog_id
+  				FROM blog_vote 
+  				WHERE blog_id='.$id.' LIMIT 1';
+  				$result3 = $db->query($sql);
+  				$row =$result3->fetch_assoc();
+
+  				$total_rows = $row['total_rows'];
+  				$total_rating = $row['total_rating'];
+  				$results['vote_rows'] = $total_rows;
+  				$rating = 0;
+  				if ($total_rows > 0) {
+  					$rating = $total_rating / $total_rows;
+  				}
+  				$dec_rating = round($rating, 1);
+				        //echo  $dec_rating;
+
+  				$starNumber=$dec_rating;
+  				for($x=1;$x<=$starNumber;$x++) {
+  					echo '<img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-128.png" style="width:35px" />';
+  				}
+  				if (strpos($starNumber,'.')) {
+  					echo '<img src="https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Star-Half-Full.png"  style="width:35px"/>';
+  					$x++;
+  				}
+  				while ($x<=5) {
+  					echo '<img src="http://time-static-shared.s3-website-us-east-1.amazonaws.com/interactives/how_american_are_you/images/white-star-md.png" style="width:35px" />';
+  					$x++;
+  				}
+
+  				?>
+  			</div></td>
+  			<td><div id="desA"><h3><?php echo $name; ?></h3></div></td>
+
+  			<td>
+  				<div id= "priceA">
+  					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $price; ?>
+  					<p></p>
+
+  					<p>
+  						<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/index/'.$id) ?>">Book Now </a>	
+  						<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/viewAll/'.$id) ?>">View all </a>
+  					</p>
+  				</div>
+  			</td>
+  		</tr>
+  		<?php
+
+  	}
+  	?>
+  </tbody>
+
+
+  <table class="table">
+
+  	<tbody>
+  		<?php
+  		foreach ($ava as $object1) {
+  			$des= $object1->Destination;
+  			$a=$object1->ImagePath;
+  			$id=$object1->Rid;
+  			$name=$object1->LodgeName;
+  			$price=$object1->Price;
+  			?>
+  			<tr>
+  				<td>
+  					<div id="picA"><img src="<?php echo base_url($a);?>" width="260" height="125"></div>
+  				</td>
+  				<td>
+  					<div id="desA"><h3><?php echo $des; ?></h3>
+  						<?php
+  						$id=$object1->Rid;
+  						$sql = 'SELECT COUNT(DISTINCT(vote_id)) total_rows,IFNULL(SUM(blog_vote),0) total_rating, blog_id
+  						FROM blog_vote 
+  						WHERE blog_id='.$id.' LIMIT 1';
+  						$result3 = $db->query($sql);
+  						$row =$result3->fetch_assoc();
+
+  						$total_rows = $row['total_rows'];
+  						$total_rating = $row['total_rating'];
+  						$results['vote_rows'] = $total_rows;
+  						$rating = 0;
+  						if ($total_rows > 0) {
+  							$rating = $total_rating / $total_rows;
+  						}
+  						$dec_rating = round($rating, 1);
+					        //echo  $dec_rating;
+
+  						$starNumber=$dec_rating;
+  						for($x=1;$x<=$starNumber;$x++) {
+  							echo '<img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-128.png" style="width:35px" />';
+  						}
+  						if (strpos($starNumber,'.')) {
+  							echo '<img src="https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Star-Half-Full.png"  style="width:35px"/>';
+  							$x++;
+  						}
+  						while ($x<=5) {
+  							echo '<img src="http://time-static-shared.s3-website-us-east-1.amazonaws.com/interactives/how_american_are_you/images/white-star-md.png" style="width:35px" />';
+  							$x++;
+  						}
+
+  						?>
+
+
+  					</div>
+  				</td>
+  				<td><div id="desA"><h3><?php echo $name; ?></h3></div></td>
+  				<td>
+  					<div id= "priceA">
+  						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <?php echo $price; ?>
+  						<p>
+
+  						</p>
+
+  						<p>
+  							<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/index/'.$id) ?>">Book Now </a>	
+  							<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/viewAll/'.$id) ?>">View all </a>
+  						</p>
+  					</div>
+
+  				</td>
+  			</tr>
+  			<?php
+
+  		}
 
 
 
-  
-  
-    <?php
-    foreach ($dat as $object) {
-    	$des= $object->Destination.'<br/>';
-      	$a=$object->ImagePath;
-      	$name=$object->LodgeName;
-      	$id=$object->Rid;
-    	?>
-    	 
-   
-    	<div id="picA"><img src="<?php echo base_url($a);?>" width="260" height="125"></div>
-     	<div id="desA"><h3><?php echo $des; ?></h3>
-     		<?php
-        	$id=$object->Rid;
-        	$sql = 'SELECT COUNT(DISTINCT(vote_id)) total_rows,IFNULL(SUM(blog_vote),0) total_rating, blog_id
-            FROM blog_vote 
-            WHERE blog_id='.$id.' LIMIT 1';
-            	$result3 = $db->query($sql);
-              	$row =$result3->fetch_assoc();
-		        
-		        $total_rows = $row['total_rows'];
-		        $total_rating = $row['total_rating'];
-		        $results['vote_rows'] = $total_rows;
-		        $rating = 0;
-		        if ($total_rows > 0) {
-		            $rating = $total_rating / $total_rows;
-		        }
-		        $dec_rating = round($rating, 1);
-		        //echo  $dec_rating;
-
-                $starNumber=$dec_rating;
-                for($x=1;$x<=$starNumber;$x++) {
-                    echo '<img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-128.png" style="width:35px" />';
-                }
-                if (strpos($starNumber,'.')) {
-                    echo '<img src="https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Star-Half-Full.png"  style="width:35px"/>';
-                    $x++;
-                }
-                while ($x<=5) {
-                    echo '<img src="http://time-static-shared.s3-website-us-east-1.amazonaws.com/interactives/how_american_are_you/images/white-star-md.png" style="width:35px" />';
-                    $x++;
-                }
-           
-			?>
-     	</div>
-
-     	<div id= "priceA">
-		        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;LKR 10 800
-		        <p></p>
-		        
-		        <p>
-		        	<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/index/'.$id) ?>">Book Now </a>	
-		         	<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/viewAll/'.$id) ?>">View all </a>
-		       </p>
-     	</div>
-    <?php
-
-    }
-	?>
-	
-
-	
-	<?php
-    foreach ($ava as $object1) {
-    	$des= $object1->Destination;
-      	$a=$object1->ImagePath;
-      	$id=$object1->Rid;
-      	$name=$object1->LodgeName;
-    	?>
-    	<div id="picA"><img src="<?php echo base_url($a);?>" width="260" height="125"></div>
-     	<div id="desA"><h3><?php echo $des; ?></h3>
-	     	<?php
-	        	$id=$object1->Rid;
-	        	$sql = 'SELECT COUNT(DISTINCT(vote_id)) total_rows,IFNULL(SUM(blog_vote),0) total_rating, blog_id
-	            FROM blog_vote 
-	            WHERE blog_id='.$id.' LIMIT 1';
-	            	$result3 = $db->query($sql);
-	              	$row =$result3->fetch_assoc();
-			        
-			        $total_rows = $row['total_rows'];
-			        $total_rating = $row['total_rating'];
-			        $results['vote_rows'] = $total_rows;
-			        $rating = 0;
-			        if ($total_rows > 0) {
-			            $rating = $total_rating / $total_rows;
-			        }
-			        $dec_rating = round($rating, 1);
-			        //echo  $dec_rating;
-
-	                $starNumber=$dec_rating;
-	                for($x=1;$x<=$starNumber;$x++) {
-	                    echo '<img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-128.png" style="width:35px" />';
-	                }
-	                if (strpos($starNumber,'.')) {
-	                    echo '<img src="https://cdn4.iconfinder.com/data/icons/pretty_office_3/256/Star-Half-Full.png"  style="width:35px"/>';
-	                    $x++;
-	                }
-	                while ($x<=5) {
-	                    echo '<img src="http://time-static-shared.s3-website-us-east-1.amazonaws.com/interactives/how_american_are_you/images/white-star-md.png" style="width:35px" />';
-	                    $x++;
-	                }
-	           
-				?>
-
-
-     	</div>
-
-     	<div id= "priceA">
-		        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;LKR 10 800
-		        <p>
-		        	
-		        </p>
-		        
-		        <p>
-		        	<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/index/'.$id) ?>">Book Now </a>	
-		        	<a class="btn btn-default pull-left" href="<?php echo base_url('index.php/Book/viewAll/'.$id) ?>">View all </a>
-		       </p>
-     	</div>
-    
-
-
-    <?php
-
-    }
-
-    
-
-	?>
+  		?>
+  	</tbody>
 
 
 
-    
-  
-     
- </div> <!-- Accomodation Catalogue-->
- <footer>End of the page</footer>
- </div>
+
+
+  </div> <!-- Accomodation Catalogue-->
+  <!-- <footer>End of the page</footer> -->
+</div>
 
 </body>
 </html>

2daf8ee7f1d02dec96e6c5a07573c2419384dd9c
LaknaGammedda <laknaharindi5@gmail.com>
2017-07-04 08:05:35 -0700

    comp

---
+++ ci_intro/application/controllers/PaymentsCon.php
@@ -0,0 +1,36 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Cbook extends CI_Controller
+{
+	function mybook()
+	{
+		$username=$this->session->userdata('username');
+		$data = array(
+	
+			'CheckIn' => $this->input->post('txtIn'),
+			'CheckOut' => $this->input->post('txtOut'),
+			'Rid' => $this->input->post('name'),
+			'TeleNo' => $this->input->post('teleNo'),
+			'username'=>$username
+			);
+			//Transfering data to Model
+
+
+			$this->db->insert('res', $data);
+
+
+
+			$no=$this->input->post('name');
+			$x="No";
+			$data1=array(
+				'Availability'=>$x,
+				
+				);
+			$this->db->where('Rid', $no);  
+			$res=$this->db->update('reservation', $data1);  
+			$this->load->view('thankyou');
+
+
+
+	}
+}

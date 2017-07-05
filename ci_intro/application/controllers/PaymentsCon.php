441c70dd6b66c35f2b29b967579bf635e5abb681
namalkanishka <kanishkanamal5@gmail.com>
2017-07-05 06:06:22 +0530

    book

--- ci_intro/application/controllers/PaymentsCon.php
+++ ci_intro/application/controllers/PaymentsCon.php
@@ -28,9 +28,6 @@ class PaymentsCon extends CI_Controller
      $this->form_validation->set_rules('cardnumber','CardNumber','required|regex_match[/^[0-9]{16}$/]');
      $this->form_validation->set_rules('CSC','CSC','required|regex_match[/^[0-9]{3}$/]');
       
-     // $this->form_validation->set_rules('pass','Password','required');
-    // $this->form_validation->set_rules('cardnumber', 'Card Number', 'trim|required|xss_clean|callback_cardnumber_validation');
-    //  $this->form_validation->set_rules('name', 'CardName', 'trim|required|xss_clean|callback_cardnumber_validation');
       if($this->form_validation->run()){
         $this->load->view('paymentthank');
         
@@ -39,16 +36,6 @@ class PaymentsCon extends CI_Controller
       }
 
 
-      //  $this->load->helper('credit_card'); // loading helper.
-      // if (checkCreditCard ($string, $cardtype, $ccerror, $ccerrortext))
-      //   {
-      //      echo "card is ok";
-      //   }
-
-      // else
-      //  {
-      //     $this->form_validation->set_message("cardnumber_validation", 'The %s is not valid.');
-      //     echo "wrong card type/number";
-      //  }
+  
   }
 }
